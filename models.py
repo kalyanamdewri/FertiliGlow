92bfde3bf00d108b45fe61a37d83967b70d7bad0
Kalyanam Dewri <73614549+kalyanamdewri@users.noreply.github.com>
2024-08-12 06:48:15 +0530

    update
    update the logging system to include additional health metrics such as diet, exercise, and sleep

--- models.py
+++ models.py
@@ -18,11 +18,15 @@ class Log(db.Model):
     mood = db.Column(db.String(64))
     temperature = db.Column(db.Float)
     notes = db.Column(db.String(200))
+    diet = db.Column(db.String(200))  # New field for diet
+    exercise = db.Column(db.String(200))  # New field for exercise
+    sleep_hours = db.Column(db.Float)  # New field for sleep
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return f'<Log {self.timestamp}>'
+
 
 class Challenge(db.Model):
     id = db.Column(db.Integer, primary_key=True)
